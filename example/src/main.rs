b758ddc37337a4e7498ea97a7be238ad9b4387de
Konstantin Kostov <konstantin@headbright.be>
2022-02-16 17:36:04 +0100

    Update the README with more usage information

--- example/src/main.rs
+++ example/src/main.rs
@@ -25,17 +25,15 @@ impl Component for Model {
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
             Msg::AddOne => {
+                // broadcast a signal whenever we handle this message
                 self.telemetry.send("addOne", None, None, None);
                 self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
                 true
             }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
         let link = ctx.link();
         html! {
             <div>
